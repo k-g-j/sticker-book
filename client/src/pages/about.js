e35842583018df03a2ed5860d85afa4c10afe6e7
Sarah Hart <sarah.hart.nc@gmail.com>
2022-05-22 12:11:41 -0400

    basic about page added

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -1,1 +1,23 @@
 import React from "react";
+
+function About() {
+
+    return (
+        <section className="my-5" >
+            <h1 id="about">About Sticker Book</h1>
+            <div className="flex-row">
+                <img src={profileImage} className="my-2" style={{ width: "25%" }} alt="cover"  />
+                <div className="my-2">
+                    <p>
+                    We are passionate about helping people achieve their dreams by creating fun acheivable goals.
+                    This sticker book allows individuals to celebrate their wins with friends, and also collect stickers 
+                    for their sticker book. Our unqiue sticker book allows individuals to look back fondly on the goals they have 
+                    achieved in the past. Keep acheiving your goals to increase your collection! 
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default About
