9d79969f06f4d24f73b7d7aca45e4212d1c11d48
Kate Johnson <johnson.kat.g@gmail.com>
2022-05-22 21:36:02 -0400

    added GoalList

--- client/src/pages/Goals.js
+++ client/src/pages/Goals.js
@@ -1,27 +1,29 @@
-import React, { useState } from 'react';
-import { QUERY_ME, QUERY_GOALS } from '../utils/queries';
-import { useQuery, useMutation } from '@apollo/client';
-import { ADD_GOAL } from '../utils/mutations';
+import React, { useState } from 'react'
+import { QUERY_ME, QUERY_GOALS } from '../utils/queries'
+import { useQuery, useMutation } from '@apollo/client'
+import { ADD_GOAL } from '../utils/mutations'
 
-import Auth from '../utils/auth';
+import Auth from '../utils/auth'
+import GoalList from '../components/GoalList'
 
 // import encouragment commponent
 //import reminder component
 
-
 const GoalsList = (props) => {
+  const [formState, setFormState] = useState({
+    goalText: '',
+    type: '',
+    steps: '',
+  })
+  const [addGoal] = useMutation(ADD_GOAL)
 
-    const [formState, setFormState] = useState({ goalText: '', type: '', steps: '' });
-    const [addGoal] = useMutation(ADD_GOAL);
+  const { loading, data } = useQuery(QUERY_ME)
 
-    const { loading, data } = useQuery(QUERY_ME);
+  const user = data?.me || {}
 
-    const user = data?.me || {};
 
-  console.log(user);
-
-if (loading) {
-    return <div>Loading...</div>;
+  if (loading) {
+    return <div>Loading...</div>
   }
   if (!user?.email) {
     return (
@@ -29,33 +31,34 @@ if (loading) {
         You need to be logged in to see this. Use the navigation links above to
         sign up or log in!
       </h4>
-    );
+    )
   }
-const handleFormSubmit = async (event) => {
-    event.preventDefault();
+  const handleFormSubmit = async (event) => {
+    event.preventDefault()
     const mutationResponse = await addGoal({
       variables: {
         goalText: formState.goalText,
         type: formState.type,
-        steps:  formState.stepBody ,
+        steps: formState.stepBody,
       },
-    });
-    const token = mutationResponse.data.addGoal.token;
-    Auth.login(token);
-  };
+    })
+    const token = mutationResponse.data.addGoal.token
+    Auth.login(token)
+  }
 
   const handleChange = (event) => {
-    const { name, value } = event.target;
+    const { name, value } = event.target
     setFormState({
       ...formState,
       [name]: value,
-    });
-  };
-//edit goal function
+    })
+  }
+  //edit goal function
+
 
   return (
-      <>
-        <form onSubmit={handleFormSubmit}>
+    <>
+      <form onSubmit={handleFormSubmit}>
         <div className="">
           <label htmlFor="goalText">Goal:</label>
           <input
@@ -66,8 +69,8 @@ const handleFormSubmit = async (event) => {
             onChange={handleChange}
           />
         </div>
-        <div className=''>
-        <label htmlFor="type">Goal Type:</label>
+        <div className="">
+          <label htmlFor="type">Goal Type:</label>
           <input
             placeholder="Type your goal here"
             name="type"
@@ -75,18 +78,16 @@ const handleFormSubmit = async (event) => {
             id="type"
             onChange={handleChange}
           />
-           </div>
-        </form>
+        </div>
+      </form>
 
-      <div className='goalList'>
-    <ul>
-      <li>
-        {user.goals.goalText}
-      </li>
-    </ul>
+      <div>
+        {user.goals.map((goal, i) => (
+          <GoalList goal={goal} key={i} />
+        ))}
       </div>
-      </>
+    </>
   )
-};
+}
 
-export default GoalsList;
+export default GoalsList
