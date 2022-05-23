34ec66525a5a8cf8947100fef399c24a43c17ee8
Sarah Hart <sarah.hart.nc@gmail.com>
2022-05-22 21:01:11 -0400

    try to update positioning

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -1,15 +1,15 @@
 import React from "react";
-import profileImage from "../assets/images/filler-profile.jpeg";
+// import profileImage from "../assets/images/filler-profile";
 
 function About() {
 
     return (
         <section className="my-5" >
-            <h1 id="about" class="font-brush">About Sticker Book</h1>
+            <h1 id="about" class="font-brush text-2xl">About Sticker Book</h1>
             <div className="flex-row">
-                <img src={profileImage} class="my-2" style={{ width: "25%" }} alt="cover"  />
+                {/* <img src={profileImage} class="my-2" style={{ width: "25%" }} alt="cover"  /> */}
                 <div className="my-2" class="font-hand">
-                    <p>
+                    <p class="text-lg">
                     We are passionate about helping people achieve their dreams by creating fun acheivable goals.
                     This sticker book allows individuals to celebrate their wins with friends, and also collect stickers 
                     for their sticker book. Our unqiue sticker book allows individuals to look back fondly on the goals they have 
