c7250bd5636bd5d4e316055c368e78ae69d1a98c
Sarah Hart <sarah.hart.nc@gmail.com>
2022-05-22 22:39:54 -0400

    background area

--- client/src/App.js
+++ client/src/App.js
@@ -51,7 +51,6 @@ function App() {
     //     </Routes>
     //   </Router>
     // </ApolloProvider>
-    
   );
 }
 
--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -4,17 +4,19 @@ import React from "react";
 function About() {
 
     return (
-        <section className="my-5" >
-            <h1 id="about" class="font-brush text-3xl font-bold">About Sticker Book</h1>
-            <div className="flex-wrap">
-                {/* <img src={profileImage} class="my-2" style={{ width: "25%" }} alt="cover"  /> */}
-                <div className="my-2" class="font-hand">
-                    <p class="text-base">
-                    We are passionate about helping people achieve their dreams by creating fun acheivable goals.
-                    This sticker book allows individuals to celebrate their wins with friends, and also collect stickers 
-                    for their sticker book. Our unqiue sticker book allows individuals to look back fondly on the goals they have 
-                    achieved in the past. Keep acheiving your goals to increase your collection! 
-                    </p>
+        <section className="my-5" class="display-flex flex-col justify-center items-center">
+            <div class="block relative border-3 border-solid border-gray-900 w-1/3 h-2/3 text-center bg-teal-600 rounded-xl">
+                <h1 id="about" class="font-brush text-3xl font-bold">About Sticker Book</h1>
+                <div className="flex-wrap">
+                    {/* <img src={profileImage} class="my-2" style={{ width: "25%" }} alt="cover"  /> */}
+                    <div className="my-2" class="">
+                        <p class="text-base font-hand">
+                        We are passionate about helping people achieve their dreams by creating fun acheivable goals.
+                        This sticker book allows individuals to celebrate their wins with friends, and also collect stickers 
+                        for their sticker book. Our unqiue sticker book allows individuals to look back fondly on the goals they have 
+                        achieved in the past. Keep acheiving your goals to increase your collection! 
+                        </p>
+                    </div>
                 </div>
             </div>
         </section>
