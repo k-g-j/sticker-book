d29e87d984194a81168b7795f6b23d15b10cb804
MrSumada <ricksamericana.com>
2022-05-24 15:44:45 -0400

    added navigation links

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,43 +1,50 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Star from '../assets/stickers/goldstar.png';
 
 //We're extracting the key value pairs from props by giving the href it's current page value
 function Nav({ currentPage, handlePageChange }) {
   return (
-    <header>
-    <ul >
-      <li>
-        <a
-          href="#home"
-          onClick={() => handlePageChange('Home')}
-      
-          //this is a Ternary operator that is checking to see if the Home nav-link is active
-          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-        >
-          Home
-        </a>
-    
-        <a
-          href="#about"
-          onClick={() => handlePageChange('About')}
-        
-          // Determine whether the About nav link is active or not
+    <header className='w-screen'>
+      <div className='flex flex-row justify-between w-screen absolute p-5'>
+        <Link to="/">
+          <img className="w-10 h-10" src={Star} alt="Sticker Book Star" />
+        </Link>
+        <ul className='flex flex-row justify-end w-1/2 px-5'>
+          <li className='pl-5'>
+            <Link 
+              to="/"
+              // onClick={() => handlePageChange('Home')}
+              //this is a Ternary operator that is checking to see if the Home nav-link is active
+              className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+            >
+              Home
+            </Link>
+          </li>
+          <li className='pl-5'>
+            <Link
+              to="/about"
+              // onClick={() => handlePageChange('About')}
+              // Determine whether the About nav link is active or not
 
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-        >
-          About
-        </a>
-        <a
-          href="#contact"
-          // Determine whether the Contact nav link is active or not
-
-          onClick={() => handlePageChange('Contact')}
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Profile
-        </a>
-       </li>
-    </ul>
-   </header>
+            className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+            >
+              About
+            </Link>
+          </li>
+          <li className='pl-5'>
+            <Link
+              to="/dashboard"
+              // Determine whether the Contact nav link is active or not
+              // onClick={() => handlePageChange('Dashboard')}
+              className={currentPage === 'Dashboard' ? 'nav-link active' : 'nav-link'}
+            >     
+              Profile
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </header>
   );
 }
 
