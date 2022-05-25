2eceaebd323212515f8594884cccdda79ed66764
Sarah Hart <sarah.hart.nc@gmail.com>
2022-05-24 22:03:17 -0400

    feed updates

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -30,10 +30,10 @@ export default function Feed() {
         <div key={i} className="flex ml-4 mb-3 flex-col" class="border-8 border-solid rounded-lg p-5">
           <h2 class="font-hand text-xl font-bold bg-white p-4 rounded-lg text-center">{goal.goalText}</h2>
           {!goal.completed ? 
-            <p className="font-hand text-base text-center">Work in progress!</p>
+            <p className="font-hand text-base text-center p-3 bg-gray-200/50 rounded-lg">Work in progress!</p>
             : <p>All done 🌟</p>
           }
-          <p class="font-brush text-base font-medium">{goal.username}</p>
+          <p class="font-brush text-base text-lg font-bold text-center p-2">{goal.username}</p>
           <span class="font-hand font-bold text-xl">{goal.encouragementCount}{loggedIn && <button className="ml-2" class="text-base hover:text-lg hover:font-bold" onClick={() => handleClick(goal._id)}>Give Encouragement!</button>}</span>
         </div>
         </div>
