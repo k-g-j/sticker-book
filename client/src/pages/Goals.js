1f6c8100827bb4d05bb6d986735d6ecd08b20305
Sarah Hart <sarah.hart.nc@gmail.com>
2022-05-24 22:15:17 -0400

    update goals page

--- client/src/pages/Goals.js
+++ client/src/pages/Goals.js
@@ -61,7 +61,7 @@ if (loading) {
 
   return (
     <>
-      <form onSubmit={handleFormSubmit}>
+      <form onSubmit={handleFormSubmit} class="object-center text-left p-20">
         <div className="" class="font-hand text-xl">
           <label htmlFor="goalText">Goal:</label>
           <input class="font-hand text-base"
