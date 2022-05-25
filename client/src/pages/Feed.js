a9ac8beba7856b5bd272b460eb859fc5722002d5
Sarah Hart <sarah.hart.nc@gmail.com>
2022-05-24 21:56:04 -0400

    updates

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -29,7 +29,7 @@ export default function Feed() {
         <div key={i} className="flex ml-4 mb-3 flex-col" class="border-8 border-solid rounded-lg p-5">
           <h2 class="font-hand text-xl font-bold bg-white p-4 rounded-lg text-center">{goal.goalText}</h2>
           {!goal.completed ? 
-            <p className="font-hand text-base">Work in progress!</p>
+            <p className="font-hand text-base text-center">Work in progress!</p>
             : <p>All done 🌟</p>
           }
           <p class="font-brush text-base font-medium">{goal.username}</p>
