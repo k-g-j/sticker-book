e0e5a9a50d1ad2f2b61996b9a571fc2707be6ecf
Sarah Hart <sarah.hart.nc@gmail.com>
2022-05-25 23:04:56 -0400

    update single goal from kate

--- client/src/pages/SingleGoal.js
+++ client/src/pages/SingleGoal.js
@@ -1,140 +1,176 @@
-import React from 'react';
-import { useQuery, useMutation } from '@apollo/client';
-import { useParams } from 'react-router-dom';
-import { QUERY_GOAL } from '../utils/queries';
-import { COMPLETE_GOAL, COMPLETE_STEP } from '../utils/mutations';
-import Auth from '../utils/auth';
-
-// import encouragment commponent
-//import reminder component
+import React, { useState } from 'react'
+import { useQuery, useMutation } from '@apollo/client'
+import { useParams } from 'react-router-dom'
+import { QUERY_GOAL } from '../utils/queries'
+import {
+  ADD_STEP,
+  COMPLETE_GOAL,
+  COMPLETE_STEP,
+  DELETE_GOAL,
+} from '../utils/mutations'
+import Auth from '../utils/auth'
+import party from 'party-js'
 
 // import stickers
-import artCross from "../assets/stickers/art-cross.png";
-import eduBrain from "../assets/stickers/edu-brain.png";
-import piggy from "../assets/stickers/finance-piggy.png";
-import mentalHealth from "../assets/stickers/mental-health.png";
-import physHealth from "../assets/stickers/phys-health.png";
+import artCross from '../assets/stickers/art-cross.png'
+import eduBrain from '../assets/stickers/edu-brain.png'
+import piggy from '../assets/stickers/finance-piggy.png'
+import mentalHealth from '../assets/stickers/mental-health.png'
+import physHealth from '../assets/stickers/phys-health.png'
 
-//steps, encouragement points, sticker up top,
+const imageMap = {
+  Personal: artCross,
+  Educational: eduBrain,
+  Financial: piggy,
+  'Physical Health': physHealth,
+  'Mental Health': mentalHealth,
+}
+
 const SingleGoal = () => {
-  const { id: goalId } = useParams();
-  const { id: stepId } = useParams();
+  const { id: goalId } = useParams()
 
-  const { loading, data } = useQuery( QUERY_GOAL, {
+  const { loading, data } = useQuery(QUERY_GOAL, {
     variables: { id: goalId },
-});
-  const [completeGoal] = useMutation(COMPLETE_GOAL);
-  const [completeStep] = useMutation(COMPLETE_STEP);
+  })
+  const [completeGoal] = useMutation(COMPLETE_GOAL)
+  const [completeStep] = useMutation(COMPLETE_STEP)
+  const [deleteGoal] = useMutation(DELETE_GOAL)
+  const [addStep] = useMutation(ADD_STEP)
+  const [stepState, setStepState] = useState('')
 
-const goal = data?.goal || {};
+  const user = data?.me || {}
+  const goal = data?.goal || {}
+  const step = data?.goal || {}
 
-if (Auth.loggedIn() && Auth.getProfile().data.email === useParams) {
-    
-  }
-  
-if (loading) {
-    return <div class="font-brush">Loading...</div>;
-  }
-const handleCompleteGoal = async (goalId) => {
-  const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-  if (!token) {
-    return false;
+  function Image({ type }) {
+    return <img src={imageMap[type]} />
   }
 
-  try {
-    await completeGoal( {
-      variables : {goalId},
-    });
-
-   
-  } catch (err) {
-    console.error(err);
-  }
-};
-const handleCompleteSTEP = async (goalId) => {
-  const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-  if (!token) {
-    return false;
+  if (Auth.loggedIn() && Auth.getProfile().data.email === useParams) {
   }
 
-  try {
-    await completeStep( {
-      variables : {goalId, stepId},
-    });
-
-   
-  } catch (err) {
-    console.error(err);
+  if (loading) {
+    return <div className="font-brush">Loading...</div>
   }
-};
+  //Party JS click event
+  const handleClickGoal = (e) => {
+    party.sparkles(e.target, {
+      count: party.variation.range(20, 40),
+    })
+    const mutationResponse = completeGoal({
+      variables: { goalId },
+    })
+  }
+
+  const handleDeleteGoal = (e) => {
+    party.confetti(e.target, {
+      count: party.variation.range(70, 40),
+    })
+    const mutationResponse = deleteGoal({
+      variables: { goalId },
+    })
+    window.location.replace("/goals")
+  }
+
+  // party.resolvableShapes["hands"] = `<img src="${hands}"/>`;
+  // Party JS click event
+  // const handleCompleteStep = (e) => {
+  //   //       party.confetti(e.target, {
+  //   //       shapes: ["hands"],
+  //   //       speed: party.variation.range(60,80)
+  //   //     });
+  //   // }
+  //   party.sparkles(e.target, {
+  //     count: party.variation.range(20, 40),
+  //   })
+  //   completeStep({
+  //     variables: { goalId},
+  //   })
+  // }
+
+  const handleAddStep = async (e) => {
+    const mutationResponse = await addStep({
+      variables: {
+        goalId,
+        stepBody: stepState,
+      },
+    })
+  }
+  const handleChange = (event) => {
+    event.preventDefault()
+    const { name, value } = event.target
+    setStepState({
+      ...stepState,
+      [name]: value,
+    })
+  }
+
   return (
     <>
-      <div className="card mb-3">
+      <div className="card mb-3 nav-padding">
         <div className="card-header">
-          <h1 className="text-teal-500">
-              Goal Text like running a marathon
-              </h1>
-    <div className="card-body">
-        <h3>
-          {goal.type}
-           Mental Health
-        </h3>
-        <img className="drag" src={artCross} style={{position: "absolute"}} alt=''/>
-                    <img className="drag" src={eduBrain} style={{position: "absolute"}}alt=''/>
-                    <img className="drag" src={piggy} style={{position: "absolute"}}alt=''/>
-                    <img className="drag" src={mentalHealth} style={{position: "absolute"}}alt=''/>
-                    <img className="drag" src={physHealth} style={{position: "absolute"}}alt=''/>
-         {/* Populate stickers based on goal types */}
-         {/* {data.goals?.map((goal) => {
-                if (goal.type === 'Physical Health') {
-                    return (
-                        <img className="drag" key={goal._id} data-goalid={goal._id} data-goal={goal.goalText} src={physHealth}/>
-                    )
-                }
-                if (goal.type === 'Mental Health') {
-                    return (
-                        <img className="drag" key={goal._id} data-goalid={goal._id} data-goal={goal.goalText} src={mentalHealth}/>
-                    )
-                }
-                if (goal.type === 'Financial') {
-                    return (
-                        <img className="drag" key={goal._id} data-goalid={goal._id}  data-goal={goal.goalText} src={piggy}/>
-                    )
-                }
-                if (goal.type === 'Educational') {
-                    return (
-                        <img className="drag" key={goal._id} data-goalid={goal._id}  data-goal={goal.goalText} src={eduBrain}/>
-                    )
-                }
-                if (goal.type === 'Personal') {
-                    return (
-                        <img className="drag" key={goal.goalId} data-goalid={goal._id}  data-goal={goal.goalText} src={artCross} />
-                    )
-                }
-                return;
-          } )} */}
+          <h1 className="text-bold">{goal.goalText}</h1>
+          <div className="card-body border">
+            {/* Populate stickers based on goal types */}
+            <h2>{goal.type}</h2>
+            {/* <Image type={goal.type} /> */}
+          </div>
+          <div className="steps">
+            {goal.steps.map((step, i) => (
+              <p key={i}>{step.stepBody}</p>
+            ))}
+            {/* <button
+              className="btn"
+              onClick={(e) => {
+                handleCompleteStep(e)
+              }}
+            >
+              {' '}
+              Complete Step{' '}
+            </button> */}
+          </div>
+          <div>
+            <button
+              className="btn"
+              onClick={(e) => {
+                handleClickGoal(e)
+              }}
+            >
+              {' '}
+              Complete Goal{' '}
+            </button>{' '}
+            <br />
+            <button
+              className="btn"
+              onClick={(e) => {
+                handleDeleteGoal(e)
+              }}
+            >
+              {' '}
+              Delete Entire Goal{' '}
+            </button>
+          </div>
         </div>
         <div>
-        <ul>
-            <li>
-                {goal.step} Step 1
-            </li>
-        </ul>
-        <button onclick={handleCompleteSTEP} type='click'> Complete Step </button>
+          <form className="p-20 " onSubmit={handleAddStep}>
+            <div className="">
+              <label htmlFor="stepBody">Add a step to your goal!</label>
+              <input
+                placeholder="Type your step here"
+                name="stepBody"
+                type="stepBody"
+                id="stepBody"
+                onChange={e => setStepState(e.target.value)}
+              />
+            </div>
+            <button className="btn border-black w-100" type="submit">
+              Submit New Step
+            </button>
+          </form>
         </div>
-        <div>
-          <p>
-            {goal.reminder} 
-            You Got this!
-          </p>
-        </div>
-        <button onclick={handleCompleteGoal}type='click'>Complete Goal</button>
-      </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default SingleGoal;
+export default SingleGoal
